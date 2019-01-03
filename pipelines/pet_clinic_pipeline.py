28ab514ec5da9b44bb24a05b506547c9d80405f1
Matt Belcher <mbelcher@thoughtworks.com>
2019-01-03 16:04:16 +0000

    Matt -updating pet clinic pipeline infra as code.

--- pipelines/pet_clinic_pipeline.py
+++ pipelines/pet_clinic_pipeline.py
@@ -1,1 +1,37 @@
+#!/usr/bin/env python
+from gomatic import *
+import os, re
+
 print "Updating PetClinic Pipeline..."
+go_server_host = re.search('https?://([a-z0-9.\-._~%]+)', os.environ['GO_SERVER_URL']).group(1)
+go_server_url = "%s:%s" % (go_server_host, "8153")
+configurator = GoCdConfigurator(HostRestClient(go_server_url))
+secret_variables = {'GCLOUD_SERVICE_KEY': 'lKD+DoKDGtCsaToW...'}
+pipeline = configurator\
+	.ensure_pipeline_group("sample")\
+	.ensure_replacement_of_pipeline("PetClinic")\
+	.set_git_material(GitMaterial("https://github.com/MattBelcher/devops-in-practice-workshop.git", branch="master", ignore_patterns=set(['pipelines/*'])))
+stage = pipeline.ensure_stage("commit")
+job = stage\
+    .ensure_job("build-and-publish")\
+    .ensure_environment_variables({'MAVEN_OPTS': '-Xmx1024m', 'GCLOUD_PROJECT_ID': 'devops-workshop-mb'})\
+    .ensure_encrypted_environment_variables(secret_variables)
+job.set_elastic_profile_id('docker-jdk')
+job.add_task(ExecTask(['./mvnw', 'clean', 'package']))
+job.add_task(ExecTask(['bash', '-c', 'docker build --tag pet-app:$GO_PIPELINE_LABEL --build-arg JAR_FILE=target/spring-petclinic-2.0.0.BUILD-SNAPSHOT.jar .']))
+job.add_task(ExecTask(['bash', '-c', 'docker login -u _json_key -p"$(echo $GCLOUD_SERVICE_KEY | base64 -d)" https://us.gcr.io']))
+job.add_task(ExecTask(['bash', '-c', 'docker tag pet-app:$GO_PIPELINE_LABEL us.gcr.io/$GCLOUD_PROJECT_ID/pet-app:$GO_PIPELINE_LABEL']))
+job.add_task(ExecTask(['bash', '-c', 'docker push us.gcr.io/$GCLOUD_PROJECT_ID/pet-app:$GO_PIPELINE_LABEL']))
+stage = pipeline.ensure_stage("deploy")
+job = stage\
+    .ensure_job("deploy")\
+    .ensure_environment_variables({'GCLOUD_ZONE': 'us-central1-a', 'GCLOUD_PROJECT_ID': 'devops-workshop-mb', 'GCLOUD_CLUSTER': 'devops-workshop-gke'})\
+    .ensure_encrypted_environment_variables(secret_variables)
+job.set_elastic_profile_id('kubectl')
+job.add_task(ExecTask(['bash', '-c', 'echo $GCLOUD_SERVICE_KEY | base64 -d > secret.json && chmod 600 secret.json']))
+job.add_task(ExecTask(['bash', '-c', 'gcloud auth activate-service-account --key-file secret.json']))
+job.add_task(ExecTask(['bash', '-c', 'gcloud container clusters get-credentials $GCLOUD_CLUSTER --zone $GCLOUD_ZONE --project $GCLOUD_PROJECT_ID']))
+job.add_task(ExecTask(['bash', '-c', './deploy.sh']))
+job.add_task(ExecTask(['bash', '-c', 'rm secret.json']))
+
+configurator.save_updated_config()
